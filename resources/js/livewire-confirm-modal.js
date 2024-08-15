b16edfaf7b1200ab42a2e320c45be3d4ed5c2cc0
rombamar <marco.rombach@de.sauter-bc.com>
2024-08-15 10:53:11 +0200

    changes to alpine store object

--- resources/js/livewire-confirm-modal.js
+++ resources/js/livewire-confirm-modal.js
@@ -2,15 +2,26 @@ document.addEventListener('alpine:init', () => {
     Alpine.store('confirmModal', {
         show: false,
         title: 'default',
+        description: 'default',
 
         toggle(){
             this.show = !this.show;
-            console.log("Show? " + this.show);
+        },
+
+        hide(){
+            this.show = false;
+        },
+
+        present(){
+            this.show = true;
         },
 
         setTitle(title) {
             this.title = title
-            console.log("Title? " + this.title);
+        },
+
+        setDescription(description) {
+            this.description = description
         }
     })
 })
@@ -24,20 +35,14 @@ function parseMethod(rawMethod){
 
     params = paramString.split(regex);
 
+    params = params.map(str => str.replace(/^['"]|['"]$/g, ''));
+
     return { method: method, params: params }
 }
 
 
 Livewire.directive('confirm-modal', ({ el, directive, component, cleanup }) => {
-    let content =  directive.expression
-
-    // The "directive" object gives you access to the parsed directive.
-    // For example, here are its values for: wire:click.prevent="deletePost(1)"
-    //
-    // directive.raw = wire:click.prevent
-    // directive.value = "click"
-    // directive.modifiers = ['prevent']
-    // directive.expression = "deletePost(1)"
+    let content =  JSON.parse(directive.expression);
 
     console.log(el);
     console.log(el.getAttribute('wire:click'));
@@ -48,14 +53,12 @@ Livewire.directive('confirm-modal', ({ el, directive, component, cleanup }) => {
         return new Promise((complete, failed)=>{
 
             document.getElementById("lvcm-ok").addEventListener("click", function(){
-                Alpine.store('confirmModal').toggle();
-                console.log('ok onclick');
+                Alpine.store('confirmModal').hide();
                 complete(true);
             });
 
             document.getElementById("lvcm-cancel").addEventListener("click", function(){
-                Alpine.store('confirmModal').toggle();
-                console.log('cancel onclick');
+                Alpine.store('confirmModal').hide();
                 complete(false);
             });
 
@@ -66,8 +69,8 @@ Livewire.directive('confirm-modal', ({ el, directive, component, cleanup }) => {
         e.preventDefault()
         e.stopImmediatePropagation()
 
-        Alpine.store('confirmModal').setTitle('Test');
-        Alpine.store('confirmModal').toggle();
+        Alpine.store('confirmModal').setTitle(content.title);
+        Alpine.store('confirmModal').present();
 
         if (!await newConfirm()) {
             console.log('cancel');
